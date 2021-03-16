7281a199ea75ece4897759bdd4ea96c3f3418cbe
sky626 <skysky626@hotmail.com>
2021-03-16 17:31:31 +0800

    less

--- lossy/models/cae_32x32x32_zero_pad_bin_less.py
+++ lossy/models/cae_32x32x32_zero_pad_bin_less.py
@@ -19,6 +19,7 @@ class CAE(nn.Module):
 
 		# 64x64x64
 		self.e_conv_1 = nn.Sequential(
+                        #1
 			nn.ZeroPad2d((1, 2, 1, 2)),
 			nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(5, 5), stride=(2, 2)),
 			nn.LeakyReLU()
@@ -26,6 +27,7 @@ class CAE(nn.Module):
 
 		# 128x32x32
 		self.e_conv_2 = nn.Sequential(
+                        #2
 			nn.ZeroPad2d((1, 2, 1, 2)),
 			nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(5, 5), stride=(2, 2)),
 			nn.LeakyReLU()
@@ -33,36 +35,35 @@ class CAE(nn.Module):
 
 		# 128x32x32
 		self.e_block_1 = nn.Sequential(
+                        #3
 			nn.ZeroPad2d((1, 1, 1, 1)),
-			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+			nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
 			nn.LeakyReLU(),
 
+                        #4
 			nn.ZeroPad2d((1, 1, 1, 1)),
 			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
 		)
 
 		# 128x32x32
 		self.e_block_2 = nn.Sequential(
+                        #5
 			nn.ZeroPad2d((1, 1, 1, 1)),
 			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
 			nn.LeakyReLU(),
 
-			nn.ZeroPad2d((1, 1, 1, 1)),
-			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
-		)
+               )
 
 		# 128x32x32
 		self.e_block_3 = nn.Sequential(
-			nn.ZeroPad2d((1, 1, 1, 1)),
-			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
-			nn.LeakyReLU(),
-
+                        #8
 			nn.ZeroPad2d((1, 1, 1, 1)),
 			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
 		)
 
 		# 32x32x32
 		self.e_conv_3 = nn.Sequential(
+                        #9
 			nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2)),
 			nn.Tanh()
 		)
@@ -71,57 +72,58 @@ class CAE(nn.Module):
 
 		# 128x64x64
 		self.d_up_conv_1 = nn.Sequential(
+                        #1
 			nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=(1, 1)),
 			nn.LeakyReLU(),
-
+                        
+                        #2
 			nn.ZeroPad2d((1, 1, 1, 1)),
 			nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=(2, 2), stride=(2, 2))
 		)
 
 		# 128x64x64
 		self.d_block_1 = nn.Sequential(
+                        #3
 			nn.ZeroPad2d((1, 1, 1, 1)),
 			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
 			nn.LeakyReLU(),
 
-			nn.ZeroPad2d((1, 1, 1, 1)),
-			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
-		)
+               )
 
 		# 128x64x64
 		self.d_block_2 = nn.Sequential(
+                        #5
 			nn.ZeroPad2d((1, 1, 1, 1)),
 			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
 			nn.LeakyReLU(),
 
-			nn.ZeroPad2d((1, 1, 1, 1)),
-			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
-		)
+                )
 
 		# 128x64x64
 		self.d_block_3 = nn.Sequential(
-			nn.ZeroPad2d((1, 1, 1, 1)),
-			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
-			nn.LeakyReLU(),
-
+                        #8
 			nn.ZeroPad2d((1, 1, 1, 1)),
 			nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
 		)
 
 		# 256x128x128
 		self.d_up_conv_2 = nn.Sequential(
+                        #9
 			nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(3, 3), stride=(1, 1)),
 			nn.LeakyReLU(),
 
+                        #10
 			nn.ZeroPad2d((1, 1, 1, 1)),
 			nn.ConvTranspose2d(in_channels=32, out_channels=256, kernel_size=(2, 2), stride=(2, 2))
 		)
 
 		# 3x128x128
 		self.d_up_conv_3 = nn.Sequential(
+                        #11
 			nn.Conv2d(in_channels=256, out_channels=16, kernel_size=(3, 3), stride=(1, 1)),
 			nn.LeakyReLU(),
 
+                        #12
 			nn.ReflectionPad2d((2, 2, 2, 2)),
 			nn.Conv2d(in_channels=16, out_channels=3, kernel_size=(3, 3), stride=(1, 1)),
 			nn.Tanh()
